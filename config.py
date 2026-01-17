371d5e13a2d2764af2e4ade82b53729406f56f06
Nino <32340544+NinoNinov@users.noreply.github.com>
2026-01-17 15:51:20 +0200

    initial commit 17 Jan

---
+++ config.py
@@ -0,0 +1,6 @@
+# Database Configuration
+DB_HOST = "stock-project-db.cf4i2c2uu2x6.eu-north-1.rds.amazonaws.com"
+DB_PORT = 3306
+DB_USER = "admin"
+DB_PASSWORD = "laplandia"
+DB_NAME = "StockProjectDB"
